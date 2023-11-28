865a8a7dfdbd1f74bcccbcb6e92fb905030c97c5
Ayeshasaifsofy <ayesha@sofy.ai>
2023-11-28 22:26:53 +0500

    fixed: updated subscriptionkey to x-sofy-auth-key

--- step.sh
+++ step.sh
@@ -4,7 +4,7 @@ set -ex
 #echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
 echo "build path "${build_path}""
 curl --location --request POST 'https://public.sofy.ai/parser-microservice/build-upload' \
---header "SubscriptionKey: ${subscription_key}" \
+--header "x-sofy-auth-key: ${subscription_key}" \
 --form "applicationFile=@"${build_path}"" \
 --form "ApplicationGUID= ${application_guid}" \
 --form "CertificateName= ${certificate_name}" \
